29ec8c4b53bf02062ec9cbc90c46b0a78e967fe6
Naudar79 <naudar79@gmail.com>
2021-02-01 15:58:52 +0100

    2.3 done !

--- 08.Javascript/01.Season-1/02-maths/03-even-square/script.js
+++ 08.Javascript/01.Season-1/02-maths/03-even-square/script.js
@@ -1,13 +1,16 @@
-
 // 02-maths/03-even-square/script.js - 2.3: squares and pairs
 
 
 (() => {
 
     document.getElementById("run").addEventListener("click", () => {
-
+        let suite = [];
         // your code here
-
+        for (let index = 1; index <= 21; index++) {
+            resu = index ** 2;
+            suite.push(resu);
+        }
+        return alert(suite);
     });
 
 })();
