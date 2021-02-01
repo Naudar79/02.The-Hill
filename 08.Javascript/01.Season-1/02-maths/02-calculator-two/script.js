47fda3998b430d66bd82e86b75f9a70afe792cfa
Naudar79 <naudar79@gmail.com>
2021-02-01 14:52:48 +0100

    2.2 done !

--- 08.Javascript/01.Season-1/02-maths/02-calculator-two/script.js
+++ 08.Javascript/01.Season-1/02-maths/02-calculator-two/script.js
@@ -1,4 +1,3 @@
-
 // 02-maths/02-calculator-two/script.js - 2.2: calculatrice (2)
 
 (() => {
@@ -6,6 +5,20 @@
 
     const performOperation = operation => {
         // perform the operation
+        switch (operation) {
+            case 'addition':
+                window.alert((new Number(document.getElementById("op-one").value)) + (new Number(document.getElementById("op-two").value)));
+                break;
+            case 'substraction':
+                window.alert((new Number(document.getElementById("op-one").value)) - (new Number(document.getElementById("op-two").value)));
+                break;
+            case 'multiplication':
+                window.alert((new Number(document.getElementById("op-one").value)) * (new Number(document.getElementById("op-two").value)));
+                break;
+            case 'division':
+                window.alert((new Number(document.getElementById("op-one").value)) / (new Number(document.getElementById("op-two").value)));
+                break;
+        }
     };
 
     Array.from(document.querySelectorAll("button.operator")).forEach($btn =>
