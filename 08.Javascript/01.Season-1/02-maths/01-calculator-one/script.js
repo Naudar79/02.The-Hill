4847a97b122d1a6c69f72c37eb87f2ce1fbc7448
Naudar79 <naudar79@gmail.com>
2021-02-01 14:30:19 +0100

    2.1 done !

--- 08.Javascript/01.Season-1/02-maths/01-calculator-one/script.js
+++ 08.Javascript/01.Season-1/02-maths/01-calculator-one/script.js
@@ -1,4 +1,3 @@
-
 // 02-maths/01-calculator-one/script.js - 2.1: calculator1
 
 (() => {
@@ -6,17 +5,25 @@
 
     document.getElementById("addition").addEventListener("click", function() {
         // perform an addition
+        let add = ((new Number(document.getElementById("op-one").value)) + (new Number(document.getElementById("op-two").value)));
+        window.alert(add);
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
         // perform an substraction
+        let sub = ((new Number(document.getElementById("op-one").value)) - (new Number(document.getElementById("op-two").value)));
+        window.alert(sub);
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
         // perform an multiplication
+        let mul = ((new Number(document.getElementById("op-one").value)) * (new Number(document.getElementById("op-two").value)));
+        window.alert(mul);
     });
 
     document.getElementById("division").addEventListener("click", function() {
         // perform an division
+        let div = ((new Number(document.getElementById("op-one").value)) / (new Number(document.getElementById("op-two").value)));
+        window.alert(div);
     });
 })();
