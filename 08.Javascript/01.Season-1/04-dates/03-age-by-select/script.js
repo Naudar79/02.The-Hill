1e6b25e90ac78331a7891011c857564b4311b48a
Naudar79 <naudar79@gmail.com>
2021-02-02 18:18:56 +0100

    4.3 done !

--- 08.Javascript/01.Season-1/04-dates/03-age-by-select/script.js
+++ 08.Javascript/01.Season-1/04-dates/03-age-by-select/script.js
@@ -4,11 +4,36 @@
 (() => {
 
     // your code here
-    function getAge(date) {
-        var diff = Date.now() - date.getTime();
-        var age = new Date(diff);
-        return Math.abs(age.getUTCFullYear() - 1970);
-    }
-    alert(getAge(new Date(1995, 12, 6))); //Date(année, mois, jour) 
 
+    //let annee = 1979;
+    //let mois = 8;
+    //let jour = 16;
+    document.getElementById("run").addEventListener("click", () => {
+
+        /* function getAge(date) {
+             let diff = Date.now() - date.getTime();
+             let age = new Date(diff);
+             return Math.abs(age.getUTCFullYear() - 1970);
+         }
+         return alert(getAge(new Date(annee, mois, jour)));
+         */
+        let annee = document.getElementById("dob-year").value;
+        let mois = document.getElementById("dob-month").value;
+        let jour = document.getElementById("dob-day").value;
+
+        let dateJ = new Date();
+        let age = dateJ.getFullYear() - annee;
+        if (mois > dateJ.getMonth() + 1) {
+            age = age - 1;
+        } else if (mois < dateJ.getMonth() + 1) {
+            age = age;
+        } else if (mois == dateJ.getMonth() + 1) {
+            if (jour <= dateJ.getDate()) {
+                age = age;
+            } else {
+                age = age - 1;
+            }
+        }
+        alert("vous avez: " + age + " ans");
+    });
 })();
