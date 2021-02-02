e5e4131039062fd9620791b1b174cf021023526f
Naudar79 <naudar79@gmail.com>
2021-02-02 11:27:07 +0100

    2.3 done !

--- 08.Javascript/01.Season-1/03-colors/02-change-bcg-two/script.js
+++ 08.Javascript/01.Season-1/03-colors/02-change-bcg-two/script.js
@@ -1,8 +1,6 @@
-
 // 03-colors/02-change-bcg-two/script.js - 3.2: Bcg 2
 
-(() => {
-
-    // your code here
-
-})();
+document.getElementById("run").addEventListener("click", () => {
+    let choix = document.getElementById("color").value;
+    return document.body.style.background = choix;
+});
