b0364284d9e53a0756741e79ddf2e84c9f4f3dcf
Naudar79 <naudar79@gmail.com>
2021-02-02 10:56:25 +0100

    3.1 done !

--- 08.Javascript/01.Season-1/03-colors/01-change-bcg-one/script.js
+++ 08.Javascript/01.Season-1/03-colors/01-change-bcg-one/script.js
@@ -1,9 +1,17 @@
-
 // 03-colors/01-change-bcg-one/script.js - 3.1: Bcg one (1)
 
+document.getElementById("red").addEventListener("click", () => {
+    return document.body.style.background = "#ff0000";
+});
 
-(() => {
+document.getElementById("green").addEventListener("click", () => {
+    return document.body.style.background = "#00ff00";
+});
 
-    // your code here
+document.getElementById("yellow").addEventListener("click", () => {
+    return document.body.style.background = "#ffc300";
+});
 
-})();
+document.getElementById("blue").addEventListener("click", () => {
+    return document.body.style.background = "#0000ff";
+});
