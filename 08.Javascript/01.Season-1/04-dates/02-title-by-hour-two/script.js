4a85a56344790c0168d4e42f5ba72465ab2bd35d
Naudar79 <naudar79@gmail.com>
2021-02-02 14:46:25 +0100

    4.2 fail !
    code is ok but vsc will not compil

--- 08.Javascript/01.Season-1/04-dates/02-title-by-hour-two/script.js
+++ 08.Javascript/01.Season-1/04-dates/02-title-by-hour-two/script.js
@@ -1,4 +1,3 @@
-
 // 04-dates/02-title-by-hour-two/script.js - 4.2: text according to the hour (2)
 
 
@@ -7,5 +6,16 @@
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
     // your code here
+
+    let now = new Date();
+    let hr = now.gethours();
+    let min = now.getMinutes();
+    if (hr < 17) {
+        document.getElementById("target").innerHTML = "Hello";
+    } else if (hr < 18 && min < 30) {
+        document.getElementById("target").innerHTML = "Hello";
+    } else {
+        document.getElementById("target").innerHTML = "Good evening";
+    }
 
 })();
