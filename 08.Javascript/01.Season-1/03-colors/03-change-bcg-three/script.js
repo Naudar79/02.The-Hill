8bfdff80983fac370ca4f6d958152fce6d3f3909
Naudar79 <naudar79@gmail.com>
2021-02-02 12:24:53 +0100

    3.3 Fail !
    debug withs Adrien D, i have same code but it will not run. need review.

--- 08.Javascript/01.Season-1/03-colors/03-change-bcg-three/script.js
+++ 08.Javascript/01.Season-1/03-colors/03-change-bcg-three/script.js
@@ -1,9 +1,14 @@
-
 // 03-colors/03-change-bcg-three/script.js - 3.3: background color (3)
 
-
 (() => {
-
-    // your code here
-
+    document.getElementById("run").addEventListener("click", () => {
+        let val = "0123456789ABCDEF";
+        let col = "#";
+        for (index = 0; index < 6; index++) {
+            col += val[Math.floor(Math.Random() * 16)];
+        }
+        return document.body.style.background = col;
+    });
 })();
+
+// WARNING : code ok but don't run !!
