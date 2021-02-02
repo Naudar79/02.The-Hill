8b9c7df4f43378e373c5361e5b291c8dac5dcf78
Naudar79 <naudar79@gmail.com>
2021-02-02 14:33:07 +0100

    4.1 fail !
    code is not compiled

--- 08.Javascript/01.Season-1/04-dates/01-title-by-hour-one/script.js
+++ 08.Javascript/01.Season-1/04-dates/01-title-by-hour-one/script.js
@@ -1,4 +1,3 @@
-
 // 04-dates/01-title-by-hour-one/script.js - 4.1: text according to the hour (1)
 
 
@@ -8,4 +7,11 @@
 
     // your code here
 
+    let now = new Date();
+    let hr = now.gethours();
+    if (hr < 18) {
+        document.getElementById("target").innerHTML = "Hello";
+    } else {
+        document.getElementById("target").innerHTML = "Good evening";
+    }
 })();
