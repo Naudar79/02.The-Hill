09a7cba0fb433df5970e1980cb3ce7af1a8f6ce1
Naudar79 <naudar79@gmail.com>
2021-02-02 09:52:20 +0100

    2.5 done !

--- 08.Javascript/01.Season-1/02-maths/05-factorial/script.js
+++ 08.Javascript/01.Season-1/02-maths/05-factorial/script.js
@@ -1,14 +1,17 @@
-
 // 02-maths/05-factorial/script.js - 2.5: Factorial
 
 (() => {
-    
+
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("run").addEventListener("click", () => {
 
         // your code here
-
+        let nombre = new Number(document.getElementById("number").value);
+        let f = 1;
+        for (index = 1; index <= nombre; index++) {
+            f *= index;
+        }
+        return alert(f);
     });
-
 })();
