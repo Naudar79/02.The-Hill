6e28a2099376b0eb42590223cd4d35e794f5dafa
Naudar79 <naudar79@gmail.com>
2021-02-03 09:56:38 +0100

    .

--- 08.Javascript/01.Season-1/04-dates/04-locale-date/script.js
+++ 08.Javascript/01.Season-1/04-dates/04-locale-date/script.js
@@ -1,4 +1,3 @@
-
 // 04-dates/04-locale-date/script.js - 4.4: textual date
 
 
@@ -7,5 +6,11 @@
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
     // your code here
+
+
+    //dim une variable "today" qui contiendra la date du jour
+    let today = new Date();
+    //afficher la variable today à la place du texte "change me !"
+    document.getElementById("target").innerHTML = today;
 
 })();
