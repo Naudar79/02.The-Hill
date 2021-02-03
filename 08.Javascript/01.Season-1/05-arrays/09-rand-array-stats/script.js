444b201f5b4ca5e80afce0c88d4c15a02e25282f
Naudar79 <naudar79@gmail.com>
2021-02-03 16:42:01 +0100

    5.9 done !

--- 08.Javascript/01.Season-1/05-arrays/09-rand-array-stats/script.js
+++ 08.Javascript/01.Season-1/05-arrays/09-rand-array-stats/script.js
@@ -3,5 +3,19 @@
 (() => {
 
     // your code here
-
+    let array = [];
+    let moy = 0;
+    document.getElementById("run").addEventListener("click", () => {
+        for (let index = 0; index < 10; index++) {
+            array.push(Math.floor(Math.random() * 100) + 1);
+        }
+        console.log(array);
+        console.log(Math.max(...array));
+        console.log(Math.min(...array));
+        let som = array.reduce((b, c) => {
+            return b + c;
+        });
+        console.log(som);
+        console.log(som / 10);
+    });
 })();
