7f8b27367b8d10b82e123d1f8db4d810b0a5d839
Naudar79 <naudar79@gmail.com>
2021-02-03 09:13:42 +0100

    clean code

--- 08.Javascript/01.Season-1/04-dates/03-age-by-select/script.js
+++ 08.Javascript/01.Season-1/04-dates/03-age-by-select/script.js
@@ -5,18 +5,8 @@
 
     // your code here
 
-    //let annee = 1979;
-    //let mois = 8;
-    //let jour = 16;
     document.getElementById("run").addEventListener("click", () => {
 
-        /* function getAge(date) {
-             let diff = Date.now() - date.getTime();
-             let age = new Date(diff);
-             return Math.abs(age.getUTCFullYear() - 1970);
-         }
-         return alert(getAge(new Date(annee, mois, jour)));
-         */
         let annee = document.getElementById("dob-year").value;
         let mois = document.getElementById("dob-month").value;
         let jour = document.getElementById("dob-day").value;
