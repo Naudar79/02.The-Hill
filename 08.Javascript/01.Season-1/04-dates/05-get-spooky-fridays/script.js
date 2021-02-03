916ebdba5fbbfedb5e13dca8844909bb9830179f
Naudar79 <naudar79@gmail.com>
2021-02-03 10:46:50 +0100

    4.5 done !

--- 08.Javascript/01.Season-1/04-dates/05-get-spooky-fridays/script.js
+++ 08.Javascript/01.Season-1/04-dates/05-get-spooky-fridays/script.js
@@ -1,9 +1,20 @@
-
 // 04-dates/05-get-spooky-fridays/script.js - 4.5: calcul des vendredi 13
 
 
 (() => {
-
+    let fri = 0;
     // your code here
+    document.getElementById("run").addEventListener("click", (annee) => {
+
+        annee = document.getElementById("year").value;
+
+        for (let index = 1; index < 12; index++) {
+            let date = new Date(annee, index, 13);
+            if (date.getDay() == 5) {
+                fri++;
+            }
+        }
+        return alert(fri);
+    });
 
 })();
