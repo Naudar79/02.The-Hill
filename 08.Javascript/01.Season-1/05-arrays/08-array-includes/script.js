6f69a07951bdb5ae3730bf4e3ae4ac2c9e0a8dac
Naudar79 <naudar79@gmail.com>
2021-02-03 15:38:58 +0100

    5.8 done !

--- 08.Javascript/01.Season-1/05-arrays/08-array-includes/script.js
+++ 08.Javascript/01.Season-1/05-arrays/08-array-includes/script.js
@@ -18,5 +18,11 @@
     ];
 
     // your code here
-
+    document.getElementById("run").addEventListener("click", () => {
+        if ((fruits.includes("apple"))) {
+            console.log("yes");
+        } else {
+            console.log("no");
+        }
+    });
 })();
