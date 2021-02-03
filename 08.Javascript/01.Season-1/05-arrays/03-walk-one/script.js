74930ada9233015fe9ab8e912f8859445dda072a
Naudar79 <naudar79@gmail.com>
2021-02-03 12:04:04 +0100

    5.3 done !

--- 08.Javascript/01.Season-1/05-arrays/03-walk-one/script.js
+++ 08.Javascript/01.Season-1/05-arrays/03-walk-one/script.js
@@ -1,4 +1,4 @@
-// 05-arrays/01-get-element/script.js - 5.1: walk through the list
+// 05-arrays/01-get-element/script.js - 5.3: walk through the list
 
 
 (() => {
@@ -17,7 +17,11 @@
         "grap",
         "cherry",
     ];
-
     // your code here
+    document.getElementById("run").addEventListener("click", () => {
+        for (index = 0; index < fruits.length; index++) {
+            console.log(fruits[index]);
+        }
+    });
 
 })();
