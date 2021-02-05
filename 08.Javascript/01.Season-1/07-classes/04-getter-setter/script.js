4b39050e5653d89110938996144c7c155a3e7871
Naudar79 <naudar79@gmail.com>
2021-02-05 12:18:06 +0100

    7.4 done !

--- 08.Javascript/01.Season-1/07-classes/04-getter-setter/script.js
+++ 08.Javascript/01.Season-1/07-classes/04-getter-setter/script.js
@@ -3,4 +3,22 @@
 
 (() => {
     // your code here
+    class Person {
+        constructor(firstName, lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+        get name() {
+            return this.firstName + " " + this.lastName;
+        }
+        set name(add) {
+            [this.firstName, this.lastName] = add.split(" ");
+        }
+    }
+    document.getElementById("run").addEventListener("click", () => {
+        let newEntr = new Person("Arnaud", "Want");
+        console.log(newEntr.name);
+        newEntr.name = "Arnaud Want";
+        console.log(newEntr.name);
+    });
 })();
