a59d29556f956194278437b1494bf924b2165598
Naudar79 <naudar79@gmail.com>
2021-02-05 10:10:08 +0100

    7.4 done !

--- 08.Javascript/01.Season-1/07-classes/02-methods/script.js
+++ 08.Javascript/01.Season-1/07-classes/02-methods/script.js
@@ -3,4 +3,24 @@
 
 (() => {
     // your code here
+    class person {
+        constructor(firstName, lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+        get sayHello() {
+            let retour = ("hello " + this.firstName + " " + this.lastName);
+            return (retour);
+        }
+    }
+    document.getElementById("run").addEventListener("click", () => {
+        let entree = new person("Arnaud", "Want");
+        console.log(entree.sayHello);
+    });
 })();
+
+// *******************************************************************
+// * 7.2 this exercise will create a class withs two properties and  *
+// * une method. on click create a instance of person class and      *
+// * return the mehtod into the consol                               *
+// *******************************************************************
