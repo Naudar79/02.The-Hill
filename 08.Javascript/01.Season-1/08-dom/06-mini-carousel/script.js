32b96a09e4ce731098197484cee04844c6011737
Naudar79 <naudar79@gmail.com>
2021-02-06 15:03:34 +0100

    8.6 done !

--- 08.Javascript/01.Season-1/08-dom/06-mini-carousel/script.js
+++ 08.Javascript/01.Season-1/08-dom/06-mini-carousel/script.js
@@ -3,7 +3,7 @@
 
 (() => {
 
-    let gallery= [
+    let gallery = [
         "../../_shared/img/bell.svg",
         "../../_shared/img/clock.svg",
         "../../_shared/img/compass.svg",
@@ -12,5 +12,15 @@
     ];
 
     // your code here
+    let cpt = 0;
+    let image = document.getElementsByTagName("img")[0];
+
+    document.getElementById("next").addEventListener("click", () => {
+        cpt = cpt + 1;
+        if (cpt > gallery.length - 1) {
+            cpt = 0;
+        }
+        image.src = gallery[cpt];
+    });
 
 })();
