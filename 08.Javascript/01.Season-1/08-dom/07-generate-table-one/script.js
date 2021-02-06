ea9a32aa5fc45834e95e5c697986c4396f795b8f
Naudar79 <naudar79@gmail.com>
2021-02-06 18:02:02 +0100

    8.7 done !

--- 08.Javascript/01.Season-1/08-dom/07-generate-table-one/script.js
+++ 08.Javascript/01.Season-1/08-dom/07-generate-table-one/script.js
@@ -4,5 +4,12 @@
 (() => {
 
     // your code here
-
+    let table = document.createElement("table");
+    for (let index = 1; index <= 10; index++) {
+        let tr = document.createElement("tr");
+        let td = document.createElement("td");
+        tr.appendChild(td);
+        table.appendChild(tr);
+    }
+    document.getElementById("target").appendChild(table);
 })();
