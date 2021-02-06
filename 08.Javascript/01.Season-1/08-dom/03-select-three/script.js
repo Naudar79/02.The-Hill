a6c44df62e2fd7f53fbf6ebb2a0c115e14333444
Naudar79 <naudar79@gmail.com>
2021-02-06 14:07:04 +0100

    8.3 fail ! need CR

--- 08.Javascript/01.Season-1/08-dom/03-select-three/script.js
+++ 08.Javascript/01.Season-1/08-dom/03-select-three/script.js
@@ -4,5 +4,9 @@
 (() => {
 
     // your code here
-
+    let array = document.querySelectorAll("target");
+    for (let index = 0; index < array.length; index++) {
+        array.forEach(element => element.innerHTML("Owned"));
+        tab[index].innerHTML = "Owned";
+    }
 })();
