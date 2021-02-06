713a3fdb0fd520078dc7d1146c76612c522852dc
Naudar79 <naudar79@gmail.com>
2021-02-06 18:44:45 +0100

    8.8 done !

--- 08.Javascript/01.Season-1/08-dom/08-generate-table-two/script.js
+++ 08.Javascript/01.Season-1/08-dom/08-generate-table-two/script.js
@@ -3,5 +3,15 @@
 (() => {
 
     // your code here
-
+    let table = document.createElement("table");
+    for (let i = 1; i <= 10; i++) {
+        let tr = document.createElement("tr");
+        for (let j = 1; j <= 10; j++) {
+            let td = document.createElement("td");
+            td.innerHTML = i * j;
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+    document.getElementById("target").appendChild(table);
 })();
